2283ac1cf77155dbe368d74f435a3146d648f61d
yasir-rtx <yasir112358@gmail.com>
2024-02-23 15:29:07 +0700

    Grasshopper - Personalized Message - UPDATE
    February 018/366

--- #18.js
+++ #18.js
@@ -1,9 +1,7 @@
 // 018/366
 // https://www.codewars.com/kata/5772da22b89313a4d50012f7
 
-function greet(name, owner) {
-    // Add code here
-}
+const greet = (name, owner) => (name === owner ? "Hello boss" : "Hello guest");
 
 console.log(greet("Daniel", "Daniel"));
 console.log(greet("Greg", "Daniel"));
