f3d67a153810211360102e66b3b4672f4da6ccb6
yasir-rtx <yasir112358@gmail.com>
2024-02-23 15:28:36 +0700

    Grasshopper - Personalized Message
    February 018/366

---
+++ #18.js
@@ -0,0 +1,9 @@
+// 018/366
+// https://www.codewars.com/kata/5772da22b89313a4d50012f7
+
+function greet(name, owner) {
+    // Add code here
+}
+
+console.log(greet("Daniel", "Daniel"));
+console.log(greet("Greg", "Daniel"));
