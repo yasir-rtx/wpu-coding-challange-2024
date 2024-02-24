761fdb7b2a0d07fcf56b58f55cea1d0a2b63ec2f
yasir <yasir112358@gmail.com>
2024-02-24 22:55:27 +0700

    Cleaning Repo

--- #17.js
+++
@@ -1,9 +0,0 @@
-// 017/366
-// https://www.codewars.com/kata/5ab6538b379d20ad880000ab
-
-const areaOrPerimeter = function (l, w) {
-    return l == w ? l * w : 2 * (l + w);
-};
-
-console.log(areaOrPerimeter(3, 3));
-console.log(areaOrPerimeter(6, 10));
--- README.md
+++
@@ -1,1 +0,0 @@
-Repo for wpu coding challange 2024
