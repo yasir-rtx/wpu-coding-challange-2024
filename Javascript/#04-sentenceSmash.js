a37ff52cd0618b2f49a9b3641d5ce21558121057
yasir-rtx <yasir112358@gmail.com>
2024-02-24 11:48:16 +0700

    Clear Repo

--- Javascript/#04-sentenceSmash.js
+++
@@ -1,6 +0,0 @@
-// 004/366
-// https://codewars.com/kata/53dc23c68a0c93699800041d
-
-const smash = (words) => words.join(" ");
-
-console.log(smash(["hello", "world"]));
--- Javascript/#18.js
+++
@@ -1,7 +0,0 @@
-// 018/366
-// https://www.codewars.com/kata/5772da22b89313a4d50012f7
-
-const greet = (name, owner) => (name === owner ? "Hello boss" : "Hello guest");
-
-console.log(greet("Daniel", "Daniel"));
-console.log(greet("Greg", "Daniel"));
